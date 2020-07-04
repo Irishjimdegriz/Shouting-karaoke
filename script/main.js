32a425a54f003ca7c607ababdba2c3b33db5cb7e
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-07-04 18:31:08 +0300

    feat: moving background and character

--- script/main.js
+++ script/main.js
@@ -1,2 +1,54 @@
 'use strict';
 
+class Obstacle {
+  constructor(x, height, rotated, width) {
+    this.x = x;
+    this.height = height;
+    this.rotated = rotated;
+    //this.width = width;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  class Level {
+    constructor(lyrics, obstacles, speed, length) {
+      this.lyrics = [];
+      this.obstacles = [];
+      this.speed = 1;
+      this.animationFrame = null;
+      this.scrollLeftOffset = 0;
+      this.length = 10000;
+    }
+
+    init() {
+      const gameContainer = document.querySelector('.game-container');
+
+      document.addEventListener('keydown', (event) => {
+        if (event.code === 'Space') {
+          if (this.animationFrame === null) {
+            this.animationFrame = requestAnimationFrame(this.animateGame.bind(this));
+          } else {
+            cancelAnimationFrame(this.animationFrame);
+            this.animationFrame = null;
+          }
+        }
+      });
+    }
+
+    animateGame() {
+      this.scrollLeftOffset += 10;
+      const screenContainer = document.querySelector('.screen-container'),
+            player = document.querySelector('.player');
+
+      screenContainer.scrollLeft = this.scrollLeftOffset;
+      player.style.left = `${this.scrollLeftOffset}px`;
+
+      if (this.scrollLeftOffset !== this.length) {
+        this.animationFrame = requestAnimationFrame(this.animateGame.bind(this));
+      }
+    }
+  }
+
+  const level = new Level();
+  level.init();
+});
